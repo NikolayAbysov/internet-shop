399a92c1e32391a841a1e9f91e17ff5ece0e8dcc
NikolayAbysov <nikolay.abysov@ukr.net>
2020-05-06 16:50:20 +0300

    Log4j functionality implemented

--- src/main/java/mate/academy/internetshop/controller/LoginController.java
+++ src/main/java/mate/academy/internetshop/controller/LoginController.java
@@ -10,9 +10,12 @@ import mate.academy.internetshop.exception.AuthenticationException;
 import mate.academy.internetshop.lib.Injector;
 import mate.academy.internetshop.model.User;
 import mate.academy.internetshop.security.AuthenticationService;
+import mate.academy.internetshop.web.filter.AuthenticationFilter;
+import org.apache.log4j.Logger;
 
 public class LoginController extends HttpServlet {
     private static final Injector INJECTOR = Injector.getInstance("mate.academy.internetshop");
+    private static final Logger LOGGER = Logger.getLogger(AuthenticationFilter.class);
     private AuthenticationService authenticationService
             = (AuthenticationService) INJECTOR.getInstance(AuthenticationService.class);
 
@@ -32,6 +35,7 @@ public class LoginController extends HttpServlet {
             HttpSession session = req.getSession();
             session.setAttribute("user_id", user.getId());
         } catch (AuthenticationException e) {
+            LOGGER.warn("Authentication failed for user :" + login + " Stack trace: " + e.getMessage());
             req.setAttribute("errorMessage", e.getMessage());
             req.getRequestDispatcher("/WEB-INF/views/users/login.jsp").forward(req, resp);
             return;
